14fee7de827dab3c6e0d0db75972c20b81862cbf
Granzou <loopinf@gmail.com>
2022-09-23 11:07:15 +0900

    comment out unnecessary initialized import

--- oslo/transformers/__init__.py
+++ oslo/transformers/__init__.py
@@ -1,10 +1,10 @@
-from oslo.transformers.models.gpt2.modeling_gpt2 import (
-    GPT2Model,
-    GPT2LMHeadModel,
-    GPT2DoubleHeadsModel,
-    GPT2ForSequenceClassification,
-    GPT2ForTokenClassification,
-)
+# from oslo.transformers.models.gpt2.modeling_gpt2 import (
+#     GPT2Model,
+#     GPT2LMHeadModel,
+#     GPT2DoubleHeadsModel,
+#     GPT2ForSequenceClassification,
+#     GPT2ForTokenClassification,
+# )
 
 
 from oslo.transformers.training_args import TrainingArguments
