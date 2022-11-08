f91f4a1b4ccc5fdae25173821191d05ea95793f8
scsc0511 <scsc0511@naver.com>
2022-11-08 14:46:46 +0000

    [#82] implement deparallelization feature for expert_parallel

--- tests/torch/nn/parallel/expert_parallel/wrapper_test/init_test/gpt2_deparallelize.py
+++ tests/torch/nn/parallel/expert_parallel/wrapper_test/init_test/gpt2_deparallelize.py
@@ -9,10 +9,7 @@ import torch
 import torch.nn as nn
 import torch.multiprocessing as mp
 
-from torch.optim import Adam
-from torch.utils.data import DataLoader
-
-from oslo.torch.distributed import ParallelContext
+from oslo.torch.distributed import ParallelContext, ParallelMode
 from oslo.torch.nn.parallel.expert_parallel.expert_parallel import _ExpertParallel
 from oslo.torch.nn.parallel.data_parallel.data_parallel import DistributedDataParallel
 
@@ -24,11 +21,11 @@ import wandb
 
 os.environ["TRANSFORMER_CACHE"] = "/fsx/seokung/.cache"
 
+save_path = "./gpt2_deparallelize.ckt"
+# torch.set_printoptions(threshold=10_000)
 
-torch.set_printoptions(threshold=10_000)
-
-world_size = 4
-expert_parallel_size = 2
+world_size = 8
+expert_parallel_size = 4
 num_experts = {
     (0,): expert_parallel_size,
     (1,): expert_parallel_size,
@@ -47,7 +44,7 @@ top_k = 1
 use_residual = True
 
 
-def run_test(rank, port):
+def run_test(rank, port, mode):
     # 1. Configure for Parallelization
     os.environ["RANK"] = str(rank)
     os.environ["LOCAL_RANK"] = str(rank)
@@ -56,8 +53,8 @@ def run_test(rank, port):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = str(port)
 
-    if rank == 0:
-        wandb.init(project="oslo", name="ep")
+    # if rank == 0 and mode != 'save':
+    #    wandb.init(project="oslo", name="ep")
 
     # 2. Set Parallel Context
     parallel_context = ParallelContext.from_torch(
@@ -66,6 +63,8 @@ def run_test(rank, port):
         tensor_parallel_size=1,
         expert_parallel_size=expert_parallel_size,
     )
+    dp_rank = parallel_context.get_local_rank(ParallelMode.DATA)
+    ep_rank = parallel_context.get_local_rank(ParallelMode.EXPERT)
 
     # 3. Create Tokenizer
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
@@ -82,52 +81,31 @@ def run_test(rank, port):
         top_k=1,
         use_residual=use_residual,
     ).to(rank)
-    wrapper_ep = DistributedDataParallel(wrapper_ep, parallel_context=parallel_context)
 
-    # 6. Prepare Optimizer
-    optimizer = Adam(wrapper_ep.parameters(), lr=3e-5)
+    # 6. Deparallelize
+    if mode == "deparallelize":
+        wrapper_ep.deparallelize()
+        if dp_rank != 0 or ep_rank != 0:
+            return
+    elif mode != "parallelize":
+        raise NotImplementedError(f"{mode} is not supported for test deparallization")
 
-    # 7. Prepare Dataset
-    datasets = load_dataset("squad").data["train"]["context"]
-    datasets = [str(sample) for sample in datasets[:500]]
-    dataloader = DataLoader(datasets, batch_size=4)
-
-    # 8. Check Parameter
-    # for param_name, module in wrapper_ep.named_parameters():
-    #    print(
-    #         f"Worker #{rank} - param_name : {param_name}, param_size : {module.size()}"
-    #    )
-    #    print(f"Worker #{rank} - param  : {module}")
-    # if wrapper_ep.expert_parallel_mapping.is_front_parallel(
-    #    wrapper_ep.model, param_name
-    # ) or wrapper_ep.expert_parallel_mapping.is_behind_parallel(
-    #    wrapper_ep.model, param_name
-    # ):
-    #    print(
-    #        f"Worker #{rank} - param_name : {param_name}, param_size : {module.size()}"
-    #    )
-    #    print(f"Worker #{rank} - param  : {module}")
-
-    # 9. Forward Propagation
-    for i, data in enumerate(dataloader):
-        start = time.time()
-        optimizer.zero_grad()
-
-        inputs = tokenizer(
-            data, return_tensors="pt", padding=True, truncation=True, max_length=512
-        ).to(rank)
-        loss = wrapper_ep(**inputs, labels=inputs["input_ids"]).loss
-        if rank == 0:
-            print(f"Rank #{rank} Iteration #{i} loss : {loss}")
-            wandb.log({"iter": i, "ep_loss": loss, "iter_time": time.time() - start})
-
-        loss.backward()
-        optimizer.step()
+    # 7. Check Parameter
+    for param_name, module in wrapper_ep.named_parameters():
+        if "expert" not in param_name:
+            continue
+        print(
+            f"Worker #{rank} - param_name : {param_name}, param_size : {module.size()}"
+        )
+        print(f"Worker #{rank} - param  : {module}")
 
 
 def test_expert_parallel_block():
-    run_func = partial(run_test, port=29500)
-    mp.spawn(run_func, nprocs=world_size)
+    # run_parallel = partial(run_test, port=29500, mode='parallelize')
+    # mp.spawn(run_parallel, nprocs=world_size)
+
+    run_deparallel = partial(run_test, port=29500, mode="deparallelize")
+    mp.spawn(run_deparallel, nprocs=world_size)
 
 
 if __name__ == "__main__":
