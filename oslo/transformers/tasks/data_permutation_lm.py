a302605fb6f6586a44bb9655cafac627160fbf2a
JinWoo Yoon <jinwoo@JinWooui-MacBook-Pro.local>
2023-02-23 19:59:43 +0900

    add permutation language modeling task referring to huggingface

---
+++ oslo/transformers/tasks/data_permutation_lm.py
@@ -0,0 +1,200 @@
+import logging
+import warnings
+from typing import Dict, List, Any, Tuple
+
+import numpy as np
+import torch
+from datasets.arrow_dataset import Batch
+from transformers import PreTrainedTokenizerBase
+
+from oslo.transformers.tasks.data_base import BaseProcessor
+
+try:
+    from transformers.tokenization_utils_base import BatchEncoding
+except ImportError:
+    print("You have to install `transformers` to use `oslo.transformers` modules")
+
+logger = logging.getLogger(__name__)
+logging.captureWarnings(True)
+
+class ProcessorForPermutationLM(BaseProcessor):
+    def __init__(self, tokenizer: PreTrainedTokenizerBase, max_length: int = 512) -> None:
+        super().__init__(tokenizer=tokenizer, max_length=max_length)
+        self._chunk_size = max_length
+
+    def __call__(self, examples: Batch) -> Dict[str, List[int]]:
+        column_names = [k for k, v in examples.items()]
+        assert (
+            "text" in column_names
+        ), "The name of dataset column that you want to tokenize must be 'text'"
+
+        dict_of_training_examples: Dict[str, List[int]] = {}
+
+        list_of_input_ids: List[List[int]] = self._tokenizer(
+            examples["text"],
+            padding=False,
+            truncation=False,
+            return_attention_mask=False,
+            return_special_tokens_mask=False,
+            add_special_tokens=False,
+            verbose=False,
+        )["input_ids"]
+
+        for input_ids in list_of_input_ids:
+            input_ids += [self._tokenizer.eos_token_id]
+            self._buffer.extend(input_ids)
+
+            while len(self._buffer) >= self._chunk_size:
+                chunk_ids = self._buffer[: self._chunk_size]
+                training_example = self._tokenizer.prepare_for_model(
+                    chunk_ids,
+                    padding=False,
+                    truncation=False,
+                    return_attention_mask=False,
+                    return_token_type_ids=False,
+                    add_special_tokens=False,
+                )
+
+                for key in training_example.keys():
+                    if key not in dict_of_training_examples:
+                        dict_of_training_examples.setdefault(key, [])
+                    dict_of_training_examples[key].append(training_example[key])
+
+                self._buffer = self._buffer[self._chunk_size :]
+
+        return dict_of_training_examples
+
+class DataCollatorForPermutationLM(object):
+    """
+    Processing training examples to mini-batch for XLNet pretraining (permutation language modeling).
+    """
+    
+    def __init__(
+        self, 
+        processor: ProcessorForPermutationLM, 
+        label_pad_token_id: int = -100, 
+        plm_probability: float = 0.15,
+        max_span_length: int = 5 # maximum length of a span of masked tokens
+    ) -> None:
+        assert isinstance(
+            processor, ProcessorForPermutationLM
+        ), "DataCollatorForPermutationLM is suitable for ProcessorForPermutationLM."
+
+        if processor._tokenizer.pad_token is None:
+            warnings.warn(
+                "If pad token doesn't exist in the processor._tokenizer, it can be a problem when applying padding."
+            )
+
+        self.tokenizer = processor._tokenizer
+        self.plm_probability = plm_probability
+        self.max_span_length = max_span_length
+        self.label_pad_token_id = label_pad_token_id
+
+    def __call__(self, examples: List[Dict[str, Any]]) -> Dict[str, torch.Tensor]:
+        # convert list to dict and tensorize input
+        batch = BatchEncoding(
+            {
+                k: np.array([examples[i][k] for i in range(len(examples))])
+                for k, v in examples[0].items()
+            }
+        )
+        input_ids = batch["input_ids"]
+        batch["input_ids"], batch["perm_mask"], batch["target_mapping"], batch["labels"] = self.mask_tokens(input_ids)
+        return batch    
+    
+    def mask_tokens(self, inputs: Any) -> Tuple[Any, Any, Any, Any]:
+        """
+        The masked tokens to be predicted for a particular sequence are determined by the following algorithm:
+            0. Start from the beginning of the sequence by setting `cur_len = 0` (number of tokens processed so far).
+            1. Sample a `span_length` from the interval `[1, max_span_length]` (length of span of tokens to be masked)
+            2. Reserve a context of length `context_length = span_length / plm_probability` to surround span to be
+               masked
+            3. Sample a starting point `start_index` from the interval `[cur_len, cur_len + context_length -
+               span_length]` and mask tokens `start_index:start_index + span_length`
+            4. Set `cur_len = cur_len + context_length`. If `cur_len < max_len` (i.e. there are tokens remaining in the
+               sequence to be processed), repeat from Step 1.
+        """
+
+        if self.tokenizer.mask_token is None:
+            raise ValueError(
+                "This tokenizer does not have a mask token which is necessary for permutation language modeling."
+                " Please add a mask token if you want to use this tokenizer."
+            )
+
+        if inputs.size(1) % 2 != 0:
+            raise ValueError(
+                "This collator requires that sequence lengths be even to create a leakage-free perm_mask. Please see"
+                " relevant comments in source code for details."
+            )
+
+        labels = inputs.clone()
+        # Creating the mask and target_mapping tensors
+        masked_indices = torch.full(labels.shape, 0, dtype=torch.bool)
+        target_mapping = torch.zeros((labels.size(0), labels.size(1), labels.size(1)), dtype=torch.float32)
+
+        for i in range(labels.size(0)):
+            # Start from the beginning of the sequence by setting `cur_len = 0` (number of tokens processed so far).
+            cur_len = 0
+            max_len = labels.size(1)
+
+            while cur_len < max_len:
+                # Sample a `span_length` from the interval `[1, max_span_length]` (length of span of tokens to be masked)
+                span_length = torch.randint(1, self.max_span_length + 1, (1,)).item()
+                # Reserve a context of length `context_length = span_length / plm_probability` to surround the span to be masked
+                context_length = int(span_length / self.plm_probability)
+                # Sample a starting point `start_index` from the interval `[cur_len, cur_len + context_length - span_length]` and mask tokens `start_index:start_index + span_length`
+                start_index = cur_len + torch.randint(context_length - span_length + 1, (1,)).item()
+                masked_indices[i, start_index : start_index + span_length] = 1
+                # Set `cur_len = cur_len + context_length`
+                cur_len += context_length
+
+            # Since we're replacing non-masked tokens with -100 in the labels tensor instead of skipping them altogether,
+            # the i-th predict corresponds to the i-th token.
+            target_mapping[i] = torch.eye(labels.size(1))
+
+        special_tokens_mask = torch.tensor(
+            [self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()],
+            dtype=torch.bool,
+        )
+        masked_indices.masked_fill_(special_tokens_mask, value=0.0)
+        if self.tokenizer._pad_token is not None:
+            padding_mask = labels.eq(self.tokenizer.pad_token_id)
+            masked_indices.masked_fill_(padding_mask, value=0.0)
+
+        # Mask indicating non-functional tokens, where functional tokens are [SEP], [CLS], padding, etc.
+        non_func_mask = ~(padding_mask | special_tokens_mask)
+
+        inputs[masked_indices] = self.tokenizer.mask_token_id
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        perm_mask = torch.zeros((labels.size(0), labels.size(1), labels.size(1)), dtype=torch.float32)
+
+        for i in range(labels.size(0)):
+             # Generate permutation indices i.e. sample a random factorisation order for the sequence. This will
+            # determine which tokens a given token can attend to (encoded in `perm_mask`).
+            # Note: Length of token sequence being permuted has to be less than or equal to reused sequence length
+            # (see documentation for `mems`), otherwise information may leak through due to reuse. In this implementation,
+            # we assume that reused length is half of sequence length and permutation length is equal to reused length.
+            # This requires that the sequence length be even.
+            
+            # Create a linear factorisation order
+            perm_index = torch.arange(labels.size(1))
+            # Split this into two halves, assuming that half the sequence is reused each time
+            perm_index = perm_index.reshape((-1, labels.size(1) // 2)).transpose(0, 1)
+            # Permute the two halves such that they do not cross over
+            perm_index = perm_index[torch.randperm(labels.size(1) // 2)]
+            # Flatten this out into the desired permuted factorisation order
+            perm_index = torch.flatten(perm_index.transpose(0, 1))
+            # Set the permutation indices of non-masked (non-functional) tokens to the
+            # smallest index (-1) so that:
+            # (1) They can be seen by all other positions
+            # (2) They cannot see masked positions, so there won't be information leak
+            perm_index.masked_fill_(~masked_indices[i] & non_func_mask[i], -1)
+            # The logic for whether the i-th token can attend on the j-th token based on the factorisation order:
+            # 0 (can attend): If perm_index[i] > perm_index[j] or j is neither masked nor a functional token
+            # 1 (cannot attend): If perm_index[i] <= perm_index[j] and j is either masked or a functional token
+            perm_mask[i] = (
+                perm_index.reshape((labels.size(1), 1)) <= perm_index.reshape((1, labels.size(1)))
+            ) & masked_indices[i]
+
+        return inputs.long(), perm_mask, target_mapping, labels.long()
