3fe086ff76b5b8061257c93448b0f0e1ae6e9d79
YH <100389977+yhna940@users.noreply.github.com>
2023-02-23 21:03:23 +0900

    [Add] support ZeRO Optimizer (#133)
    # Title
    
    Colossal AI-based LowLevel zero optimizer with PyTorch interface
    
    # Description
    
    This implementation is a temporary solution to synchronize with Oslo dp.
    Thank you for your feedback. The existing Colossal AI low-level zero
    optimizer was introduced almost as is, but the grad scaler was excluded
    from this integration due to difficulties in aligning the interface.
    Currently, there is no ddp model wrapper for Oslo, so PyTorch ddp was
    used for experimentation.
    
    ## Usage
    
    ```
    from torch.nn.parallel import DistributedDataParallel
    from oslo.torch.distributed.parallel_context import ParallelContext
    from oslo.torch.nn.parallel.data_parallel.zero import ZeroRedundancyOptimizer
    
    parallel_context = ParallelContext.from_torch(data_parallel_size=world_size)
    model = ToyModel().to(rank)
    ddp_model = DistributedDataParallel(model, device_ids=[rank])
    optimizer = optim.SGD(model.parameters(), lr=0.001)
    zero_optimizer = ZeroRedundancyOptimizer(optimizer, parallel_context=parallel_context)
    zero_optimizer.zero_grad()
    outputs = ddp_model(torch.zeros(20, 10).to(rank))
    labels = torch.zeros(20, 5).to(rank)
    loss_fn(outputs, labels).backward()
    zero_optimizer.step()
    ```
    
    
    ## Limitations
    
    1. Limitations in the original code itself:
    1.1. fp16 master weights.
    1.2. contiguous gradients.
    1.3. ~~cpu offload.~~
    1.4. support when some parameters require_grad = False.
    
    2. A new problem arose during this integration process:
    2.1. What if there are parameters that do not contribute to the loss
    function?
    
    ## Linked Issues
    
    N/A
    
    ## Todo
    
    - [x] Try mixing it with other parallelization methods.
    
    ---------
    
    Co-authored-by: dongsung kim <kidsung@ip-172-31-42-218.ec2.internal>
    Co-authored-by: Younghwan Na <younghwan@ip-26-0-128-198.ec2.internal>

---
+++ oslo/torch/nn/parallel/data_parallel/zero/sharded_optim/bookkeeping/__init__.py
@@ -0,0 +1,6 @@
+from .bucket_store import BucketStore
+from .gradient_store import GradientStore
+from .parameter_store import ParameterStore
+from .tensor_store import TensorBucket
+
+_all__ = ["BucketStore", "GradientStore", "ParameterStore", "TensorBucket"]
