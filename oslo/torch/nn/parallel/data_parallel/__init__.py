32dd1635e27e69568b20070569348d5339ab2e92
jinwonkim93 <jinwonkim93@github.com>
2023-03-08 11:31:01 +0900

    [Fix] fix conflict

--- oslo/torch/nn/parallel/data_parallel/__init__.py
+++ oslo/torch/nn/parallel/data_parallel/__init__.py
@@ -1,6 +1,6 @@
 from oslo.torch.nn.parallel.data_parallel.data_parallel import (
     DistributedDataParallel,
 )
-from oslo.torch.nn.parallel.data_parallel.zero import ZeroRedundancyOptimizer
+from oslo.torch.nn.parallel.data_parallel.zero import *
 
-__ALL__ = ["DistributedDataParallel", "ZeroRedundancyOptimizer"]
+__all__ = ["DistributedDataParallel", "ZeroRedundancyOptimizer"]
