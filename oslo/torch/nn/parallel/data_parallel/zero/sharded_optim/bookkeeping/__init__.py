2408bf0e6219929cca46fa781af64c304aee3bfe
jinwonkim93 <jinwonkim93@github.com>
2023-03-08 22:32:41 +0900

    [Style] change zero __all__ to __ALL__

--- oslo/torch/nn/parallel/data_parallel/zero/sharded_optim/bookkeeping/__init__.py
+++ oslo/torch/nn/parallel/data_parallel/zero/sharded_optim/bookkeeping/__init__.py
@@ -3,4 +3,4 @@ from .gradient_store import GradientStore
 from .parameter_store import ParameterStore
 from .tensor_store import TensorBucket
 
-_all__ = ["BucketStore", "GradientStore", "ParameterStore", "TensorBucket"]
+_ALL__ = ["BucketStore", "GradientStore", "ParameterStore", "TensorBucket"]
