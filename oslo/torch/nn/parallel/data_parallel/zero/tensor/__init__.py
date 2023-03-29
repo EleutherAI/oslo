46f7324d85c69893372055bb31aa71917738ee72
yhna940 <yhna940@gmail.com>
2023-03-29 23:21:38 +0900

    Add chunk manager unittest

--- oslo/torch/nn/parallel/data_parallel/zero/tensor/__init__.py
+++ oslo/torch/nn/parallel/data_parallel/zero/tensor/__init__.py
@@ -1,6 +1,9 @@
 from oslo.torch.nn.parallel.data_parallel.zero.tensor.distributed_tensor import (
     DistributedTensor,
 )
+from oslo.torch.nn.parallel.data_parallel.zero.tensor.distributed_tensor_spec import (
+    DistributedTensorSpec,
+)
 from oslo.torch.nn.parallel.data_parallel.zero.tensor.distributed_parameter import DistributedParameter
 
-__ALL__ = ["DistributedTensor", "DistributedParameter"]
+__ALL__ = ["DistributedTensor", "DistributedParameter", "DistributedTensorSpec"]
