2ec7dd2ce586446ea01204652d0e74ee5a74251c
dongsung kim <kidsung@ip-26-0-129-85.ec2.internal>
2023-05-09 20:18:52 +0000

    add inference code

--- tests/test_script/run_inference.sh
+++ tests/test_script/run_inference.sh
@@ -1,1 +1,13 @@
 ## inference shell code
+# EXAMPLE: ``sh ./run_inference.sh 4 bert-base-cased masked-lm ``
+
+NUM_GPUS=$1
+MODEL=$2
+TASK=$3
+
+python -m torch.distributed.launch \
+       --nproc_per_node="$NUM_GPUS" \
+       ../inference.py \
+       --task=$TASK \
+       --model=$MODEL \
+       --tensor_parallel_size="$NUM_GPUS"
