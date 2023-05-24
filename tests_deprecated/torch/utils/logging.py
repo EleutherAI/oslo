63bdf5f9145e5f68a7fb0509b04ae7178a01ca48
yhna940 <yhna940@gmail.com>
2023-05-25 00:24:50 +0900

    Fix minor

--- tests_deprecated/torch/utils/logging.py
+++ tests_deprecated/torch/utils/logging.py
@@ -6,15 +6,19 @@ import logging
 
 from oslo.torch.utils.logging import DistributedLogger
 
+
+def worker_fn(rank, log_file_path):
+    logger = DistributedLogger.get_instance(f"TestLogger{rank}")
+    logger.set_level("INFO")
+    logger.log_to_file(log_file_path / f"rank_{rank}.log")
+    logger.info(f"Test log message from rank {rank}")
+    assert (log_file_path / f"rank_{rank}.log").exists()
+
+
 class TestDistributedLogger(unittest.TestCase):
     def setUp(self):
         self.log_file_path = Path("./logs")
         self.log_file_path.mkdir(parents=True, exist_ok=True)
-
-    def tearDown(self):
-        if self.log_file_path.exists():
-            for file in self.log_file_path.glob('*.log'):
-                os.remove(file)
 
     def test_instance_creation(self):
         logger = DistributedLogger.get_instance("TestLogger")
@@ -22,26 +26,20 @@ class TestDistributedLogger(unittest.TestCase):
 
     def test_set_level(self):
         logger = DistributedLogger.get_instance("TestLogger")
-        logger.set_level('ERROR')
+        logger.set_level("ERROR")
         self.assertEqual(logger._logger.level, logging.ERROR)
 
     def test_log_to_file(self):
         logger = DistributedLogger.get_instance("TestLogger")
-        logger.set_level('INFO')
-        logger.log_to_file(self.log_file_path)
+        logger.set_level("INFO")
+        logger.log_to_file(self.log_file_path / "test.log")
         logger.info("Test log message")
-        self.assertTrue(self.log_file_path.exists())
-
-    def worker_fn(self, rank):
-        logger = DistributedLogger.get_instance(f"TestLogger{rank}")
-        logger.set_level('INFO')
-        logger.log_to_file(self.log_file_path / f'rank_{rank}.log')
-        logger.info(f"Test log message from rank {rank}")
-        self.assertTrue((self.log_file_path / f'rank_{rank}.log').exists())
+        self.assertTrue((self.log_file_path / "test.log").exists())
 
     def test_multiprocess_logging(self):
         num_processes = 4
-        mp.spawn(self.worker_fn, nprocs=num_processes)
+        mp.spawn(worker_fn, args=(self.log_file_path,), nprocs=num_processes)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
