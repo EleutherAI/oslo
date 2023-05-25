8be22db06bbf2e67b7b8e7480b217ef072a36299
YH <yhna940@gmail.com>
2023-05-25 21:57:50 +0900

    [Add] Distributed Logger Utility for PyTorch (#190)
    ## Title
    
    - [Add] Distributed Logger Utility for PyTorch
    
    ## Description
    
    - This PR introduces a distributed event logger class that is
    essentially based on Python's built-in `logging` module. The
    DistributedLogger class provides an easy-to-use API for logging events
    in a distributed environment using PyTorch.
    
    ## Linked Issues
    
    - N/A

--- oslo/torch/utils/__init__.py
+++ oslo/torch/utils/__init__.py
@@ -1,5 +1,6 @@
 from oslo.torch.utils.version import torch_version
 from oslo.torch.utils.common import get_free_port
 from oslo.torch.utils.random import set_seed
+from oslo.torch.utils.logging import get_dist_logger
 
-__all__ = ["get_free_port", "set_seed"]
+__all__ = ["get_free_port", "set_seed", "get_dist_logger"]
