7c543c928f88670fb67092e17511fca2fb650011
Younghwan Na <100389977+yhna940@users.noreply.github.com>
2023-07-12 14:22:30 +0900

    Add copyright for zero3

--- oslo/torch/nn/parallel/data_parallel/zero/sharded_optim/heterogeneous_optim.py
+++ oslo/torch/nn/parallel/data_parallel/zero/sharded_optim/heterogeneous_optim.py
@@ -1,4 +1,19 @@
-# this code is inspired by the DeepSpeed library and implemented with our own design from scratch
+# Copyright 2021 HPC-AI Technology Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Modified by EleutherAI on 2023.
+
 import math
 import warnings
 from enum import Enum
